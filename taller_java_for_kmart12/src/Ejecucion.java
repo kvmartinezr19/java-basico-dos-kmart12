a4739724b5e390414ccc42398374ffca9d0fb843
kvmartinezr19 <kvmartinezr19@gmail.com>
2024-08-08 13:02:19 -0500

    Cargue de archivos con la solucion al taller java dos

---
+++ taller_java_for_kmart12/src/Ejecucion.java
@@ -0,0 +1,17 @@
+public class Ejecucion {
+
+    public static void main(String[] args) {
+        int suma = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            suma = suma + i;
+        }
+        System.out.println("La sumatoria de los numeros del 1 al 10 es: " + suma);
+
+    }
+
+}
+
+
+
+
---
+++ taller_java_while_kmart12/src/Ejecucion.java
@@ -0,0 +1,15 @@
+public class Ejecucion {
+
+    public static void main(String[] args) {
+
+        int suma = 0;
+        int i = 1;
+
+        while (i <= 10) {
+            suma = suma + i;
+            i++;
+        }
+        System.out.println("La sumatoria de los numeros del 1 al 10 es: " + suma);
+
+    }
+}
